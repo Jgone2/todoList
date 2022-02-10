5d2520a1dcd78c001d406ebf8409d4b6e2252b75
Jgone2 <jgoneit@gmail.com>
2022-02-10 12:58:53 +0900

    readme 추가

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,8 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  const months = String(date.getMonth()).padStart(2, "0");
-  const days = String(date.getDay()).padStart(2, "0");
+  const months = String(date.getMonth() + 1).padStart(2, "0");
+  const days = String(date.getDate()).padStart(2, "0");
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Vanilla JS로 만들어 보는 todoList
+
+입니다.
+
+# 🛠 진행과정 🛠
+
+- [] Username 삽입 시 username + 환영문구 생성
+- [] todoList 추가시 아래에 input한 내용 추가(overflow시 scroll)
+- [] delete 기능
+- [x] 현재 날짜 및 시간 삽입
+- [x] 현재 지역의 날씨 및 온도
