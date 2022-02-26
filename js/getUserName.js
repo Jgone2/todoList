ebbe0a6f0a373140c5be62c2bb874476935c37d5
Jgone2 <jgoneit@gmail.com>
2022-02-26 12:23:12 +0900

    유저네임 입력저장

--- js/getUserName.js
+++ js/getUserName.js
@@ -1,15 +1,26 @@
+const USERNAME_KEY = "username";
+
 const loginForm = document.querySelector("#login-form");
 const inputUser = document.querySelector("#get-name");
-const showUserName = document.querySelector("#show-name");
+const showUsername = document.querySelector("#show-name");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-function showUserName(userName) {
-  showUserName.innerText = `Hi ${username}`;
-  showUserName.classList.remove("hidden");
+function showUserName(username) {
+  showUsername.innerHTML = `Hi ${username}`;
+  showUsername.classList.remove("hidden");
 }
 
-// function loginFormSubmit((event) => {
-//   event.preventDefault();
-//   loginForm.classList.add('hidden');
-//   const userName = inputUser.value;
-//   showUserName(userName);
-// });
+function loginFormSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add("hidden");
+  const username = inputUser.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  showUserName(username);
+}
+
+if (savedUsername === null) {
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", loginFormSubmit);
+} else {
+  showUserName(savedUsername);
+}
